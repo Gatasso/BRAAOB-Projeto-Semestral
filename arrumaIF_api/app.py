9e8d376454ad43e214011de4d31e9499beebe50d
Gatasso <galassoworkstation@gmail.com>
2026-06-04 16:17:17 -0300

    fix: altera logica de criacao do app, devido erro de deploy na vercel

--- arrumaIF_api/app.py
+++ arrumaIF_api/app.py
@@ -1,6 +1,20 @@
-from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from app import models
+from app.routes import usuario_bp, local_bp, equipamento_bp, solicitacao_bp
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(usuario_bp)
+app.register_blueprint(local_bp)
+app.register_blueprint(equipamento_bp)
+app.register_blueprint(solicitacao_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
